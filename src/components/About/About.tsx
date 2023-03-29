bec9f1d33540663d2bedaa971c216a12e3ec0203
B B <whatman+github2@gmail.com>
2023-03-29 13:39:00 -0700

    Update About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,11 +9,17 @@ export function AboutBlurb() {
     <div className="blurb">
       <p>
         Character Crisis is a utility to help you pick your next Fighting Game character! Right now, it is in{' '}
-        <strong>Alpha 1</strong>, please be patient as I continue to add features and data for more characters and
-        games. Alpha 1 features include Lists, Descriptions, and Stat details of Characters from various games. Alpha 2
-        will expand upon this by allowing selection of a character, showing their detailed profile next to a list of all
-        other characters from selected game(s), ordered by a selected score similarity. Future alphas will expand upon
-        stat comparisons, and Beta will focus on design and UX improvements.
+        <strong>Alpha 2</strong>. This is the final Alpha, released March 29, 2023. The upcoming will focus on design
+        and UX improvements. Please be patient as I continue to add features and data for more characters and games.
+        This project is open source, everything can be found on{' '}
+        <a href="https://github.com/bbkpr/charactercrisis">our Github!</a>
+      </p>
+      <p>
+        <strong>
+          I'm actively looking for people to help fill in stats and descriptions of more characters. I can pay a cash
+          bounty for each completed character. Please contact me at Bob's Beekeeping Emporium#2471 on Discord if you can
+          help!
+        </strong>
       </p>
     </div>
   );
